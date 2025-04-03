76084227d0dc1623129cfd896a41765eb766f797
LekKit <50500857+LekKit@users.noreply.github.com>
2025-04-03 13:56:33 +0300

    rvvm_types: Handle missing <inttypes.h>, apply clang-format

--- src/rvvm_types.h
+++ src/rvvm_types.h
@@ -10,26 +10,65 @@ file, You can obtain one at https://mozilla.org/MPL/2.0/.
 #ifndef RVVM_TYPES_H
 #define RVVM_TYPES_H
 
+#include "compiler.h"
+
 #include <stdbool.h>
-#include <stdint.h>
 #include <stddef.h>
+#include <stdint.h>
 
+#if CHECK_INCLUDE(inttypes.h, 1)
 #include <inttypes.h>
+#endif
 
-// Fix for MSVCRT printf specifier
-#if defined(_WIN32) && defined(PRIx64)
+// Fix for MSVCRT printf specifier on MinGW
+#if defined(_WIN32)
+#undef PRIx32
+#define PRIx32 "I32x"
+
 #undef PRIx64
 #define PRIx64 "I64x"
+
+#undef PRId32
+#define PRId32 "I32d"
+
+#undef PRId64
+#define PRId64 "I64d"
+
+#undef PRIu32
+#define PRIu32 "I32u"
+
+#undef PRIu64
+#define PRIu64 "I64u"
+#endif
+
+#ifndef PRIx32
+#define PRIx32 "x"
 #endif
 
 #ifndef PRIx64
 #define PRIx64 "llx"
 #endif
 
+#ifndef PRId32
+#define PRId32 "d"
+#endif
+
+#ifndef PRId64
+#define PRId64 "lld"
+#endif
+
+#ifndef PRIu32
+#define PRIu32 "u"
+#endif
+
+#ifndef PRIu64
+#define PRIu64 "llu"
+#endif
+
 #ifdef __SIZEOF_INT128__
 #define INT128_SUPPORT 1
 typedef unsigned __int128 uint128_t;
-typedef __int128 int128_t;
+typedef __int128          int128_t;
 #endif
 
 typedef uint8_t regid_t;  // Register index
