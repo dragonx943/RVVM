4204e1f262a8892f75dc7716a40681107345bfdd
LekKit <50500857+LekKit@users.noreply.github.com>
2025-04-22 23:51:17 +0300

    spinlock: Fix occasionally lost futex wakeups
    - When entering slow kernel wait path, lock claim should also mark SPINLOCK_HAS_WAITERS flag, otherwise any additional waiters will never receive their futex wakeup
    - Sleeping on a futex should not be done when spin_lock_possibly_available(), even if we failed to claim it, because now we will have a lock that is marked as having waiters even though noone actually holds it
    - Slightly optimize check in spin_try_read_lock()
    - Stricter misuse check in spin_unlock()
    - Reduce number of futex wakeups when it's well-known they're not gonna succeed in claiming the lock
    - Actually use futex wait timeout for working deadlock reporting

--- src/spinlock.c
+++ src/spinlock.c
@@ -1,5 +1,5 @@
 /*
-spinlock.c - Hybrid Spinlock
+spinlock.c - Fast hybrid reader/writer lock
 Copyright (C) 2021  LekKit <github.com/LekKit>
 
 This Source Code Form is subject to the terms of the Mozilla Public
@@ -19,18 +19,19 @@ file, You can obtain one at https://mozilla.org/MPL/2.0/.
 // Maximum allowed bounded locking time, reports a deadlock upon expiration
 #define SPINLOCK_DEADLOCK_MS  10000
 
-#define SPINLOCK_HAS_WRITER   0x00000001U
-#define SPINLOCK_HAS_READERS  0x7FFFFFFEU
-#define SPINLOCK_HAS_WAITERS  0x80000000U
+#define SPINLOCK_QUIESCENT    0x00000000U // No one holds the lock
+#define SPINLOCK_HAS_WRITER   0x00000001U // Writer holds the lock
+#define SPINLOCK_HAS_READERS  0x7FFFFFFEU // Reader(s) hold the lock
+#define SPINLOCK_HAS_WAITERS  0x80000000U // Has waiters
 
 static slow_path void spin_lock_debug_report(spinlock_t* lock, bool crash)
 {
-#ifdef USE_SPINLOCK_DEBUG
-    rvvm_warn("The lock was previously held at %s", lock->location);
+#if defined(USE_SPINLOCK_DEBUG)
+    rvvm_warn("The lock was last exclusively held at %s", lock->location);
 #else
     UNUSED(lock);
 #endif
-#ifdef RVVM_VERSION
+#if defined(RVVM_VERSION)
     rvvm_warn("Version: RVVM v" RVVM_VERSION);
 #endif
     if (crash) {
@@ -66,12 +67,27 @@ static inline bool spin_lock_possibly_available(uint32_t flag, bool writer)
     }
 }
 
-static inline bool spin_try_claim_internal(spinlock_t* lock, const char* location, bool writer)
+static inline bool spin_try_claim_lock(spinlock_t* lock, const char* location, bool writer, uint32_t waiters)
 {
     if (writer) {
-        return spin_try_lock_internal(lock, location);
+        uint32_t exp = SPINLOCK_QUIESCENT;
+        uint32_t val = SPINLOCK_HAS_WRITER | waiters;
+        if (likely(atomic_cas_uint32_ex(&lock->flag, exp, val, false, ATOMIC_ACQUIRE, ATOMIC_RELAXED))) {
+            SPINLOCK_MARK_LOCATION(lock, location);
+            return true;
+        }
+        return false;
     } else {
-        return spin_try_read_lock(lock);
+        uint32_t prev;
+        uint32_t new;
+        do {
+            prev = atomic_load_uint32_ex(&lock->flag, ATOMIC_RELAXED);
+            if (unlikely(!spin_lock_possibly_available(prev, false))) {
+                return false;
+            }
+            new = (prev + 2) | waiters;
+        } while (!atomic_cas_uint32_ex(&lock->flag, prev, new, true, ATOMIC_ACQUIRE, ATOMIC_RELAXED));
+        return true;
     }
 }
 
@@ -91,16 +107,15 @@ static bool spin_lock_try_wait_user(spinlock_t* lock, const char* location, bool
     // Spin on a lock in userspace for a few times before using a heavyweight kernel futex
     for (size_t i = 0; i < SPINLOCK_USER_RETRIES; ++i) {
         uint32_t flag = atomic_load_uint32_relax(&lock->flag);
-
         if (spin_lock_possibly_available(flag, writer)) {
-            if (spin_try_claim_internal(lock, location, writer)) {
+            if (spin_try_claim_lock(lock, location, writer, 0)) {
                 // Succesfully claimed the lock
                 return true;
+            } else {
+                // Contention is going on, fallback to kernel wait
+                return false;
             }
-            // Contention is going on, fallback to kernel wait
-            return false;
         }
-
         spin_pause_hint();
     }
     return false;
@@ -108,8 +123,8 @@ static bool spin_lock_try_wait_user(spinlock_t* lock, const char* location, bool
 
 slow_path static void spin_lock_wait_internal(spinlock_t* lock, const char* location, bool writer)
 {
-    rvtimer_t deadlock_timer       = {0};
-    bool      reset_deadlock_timer = true;
+    rvtimer_t deadlock_timer = {0};
+    bool      reset_timer    = true;
 
     if (spin_lock_try_wait_user(lock, location, writer)) {
         return;
@@ -118,37 +133,38 @@ slow_path static void spin_lock_wait_internal(spinlock_t* lock, const char* loca
     while (true) {
         uint32_t flag = atomic_load_uint32_relax(&lock->flag);
         if (spin_lock_possibly_available(flag, writer)) {
-            if (spin_try_claim_internal(lock, location, writer)) {
-                // Succesfully claimed the lock
+            if (spin_try_claim_lock(lock, location, writer, SPINLOCK_HAS_WAITERS)) {
+                // Succesfully claimed the lock, mark it for wakeup
                 return;
             }
 
             // Contention is going on, retry
-            reset_deadlock_timer = true;
-        }
-
-        // Indicate that we're waiting on this lock
-        if (!spin_flag_has_waiters(flag) && atomic_cas_uint32(&lock->flag, flag, flag | SPINLOCK_HAS_WAITERS)) {
-            flag |= SPINLOCK_HAS_WAITERS;
-        }
-
-        if (spin_flag_has_waiters(flag)) {
-            // Wait on a futex if we succesfully marked ourselves as a waiter
-            if (thread_futex_wait(&lock->flag, flag, THREAD_FUTEX_INFINITE)) {
-                // Reset deadlock timer upon noticing any forward progress
-                reset_deadlock_timer = true;
+            reset_timer = true;
+            spin_pause_hint();
+        } else {
+            if (reset_timer) {
+                reset_timer = false;
+                rvtimer_init(&deadlock_timer, 1000);
             }
-        }
 
-        if (reset_deadlock_timer) {
-            rvtimer_init(&deadlock_timer, 1000);
-            reset_deadlock_timer = false;
-        }
+            // Indicate that we're waiting on this lock
+            if (!spin_flag_has_waiters(flag) && atomic_cas_uint32(&lock->flag, flag, flag | SPINLOCK_HAS_WAITERS)) {
+                flag |= SPINLOCK_HAS_WAITERS;
+            }
 
-        if (location && rvtimer_get(&deadlock_timer) > SPINLOCK_DEADLOCK_MS) {
-            rvvm_warn("Possible %sdeadlock at %s", writer ? "" : "reader ", location);
-            spin_lock_debug_report(lock, false);
-            reset_deadlock_timer = true;
+            if (spin_flag_has_waiters(flag)) {
+                // Wait on a futex if we succesfully marked ourselves as a waiter
+                if (thread_futex_wait(&lock->flag, flag, SPINLOCK_DEADLOCK_MS * 1000000ULL)) {
+                    // Reset deadlock timer upon noticing any forward progress
+                    reset_timer = true;
+                }
+            }
+
+            if (location && rvtimer_get(&deadlock_timer) >= SPINLOCK_DEADLOCK_MS) {
+                rvvm_warn("Possible %sdeadlock at %s", writer ? "" : "reader ", location);
+                spin_lock_debug_report(lock, false);
+                reset_timer = true;
+            }
         }
     }
 }
@@ -172,8 +188,8 @@ slow_path void spin_lock_wake(spinlock_t* lock, uint32_t prev)
         rvvm_warn("Unlock of a non-locked writer lock!");
         spin_lock_debug_report(lock, true);
     } else if (spin_flag_has_waiters(prev)) {
-        // Wake all readers or waiter
-        thread_futex_wake(&lock->flag, -1);
+        // Wake a reader/writer
+        thread_futex_wake(&lock->flag, 1);
     }
 }
 
@@ -186,7 +202,7 @@ slow_path void spin_read_lock_wake(spinlock_t* lock, uint32_t prev)
         rvvm_warn("Unlock of a non-locked reader lock!");
         spin_lock_debug_report(lock, true);
     } else if (spin_flag_has_waiters(prev)) {
-        // Wake writer
+        // Wake a writer
         atomic_and_uint32(&lock->flag, ~0x80000000U);
         thread_futex_wake(&lock->flag, 1);
     }
--- src/spinlock.h
+++ src/spinlock.h
@@ -7,8 +7,8 @@ License, v. 2.0. If a copy of the MPL was not distributed with this
 file, You can obtain one at https://mozilla.org/MPL/2.0/.
 */
 
-#ifndef RVVM_SPINLOCK_H
-#define RVVM_SPINLOCK_H
+#ifndef LEKKIT_SPINLOCK_H
+#define LEKKIT_SPINLOCK_H
 
 #include "atomics.h"
 #include "compiler.h"
@@ -16,23 +16,18 @@ file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 /*
  * Lock flags meaning:
+ * 0x00000000: Quiescent state (No one holds the lock)
  * 0x00000001: Writer holds the lock
  * 0x7FFFFFFE: Reader count
  * 0x80000000: There are waiters
  */
 
-// Static initialization
-#define SPINLOCK_INIT {0}
-
-#ifdef USE_SPINLOCK_DEBUG
-#define SPINLOCK_DEBUG_LOCATION SOURCE_LINE
-#else
-#define SPINLOCK_DEBUG_LOCATION NULL
-#endif
+// Static initializer
+#define SPINLOCK_INIT ZERO_INIT
 
 typedef struct {
     uint32_t flag;
-#ifdef USE_SPINLOCK_DEBUG
+#if defined(USE_SPINLOCK_DEBUG)
     const char* location;
 #endif
 } spinlock_t;
@@ -47,11 +42,29 @@ slow_path void spin_read_lock_wake(spinlock_t* lock, uint32_t prev);
 // Initialize a lock
 static inline void spin_init(spinlock_t* lock)
 {
-    atomic_store_uint32(&lock->flag, 0);
-#ifdef USE_SPINLOCK_DEBUG
+    lock->flag = 0x0;
+#if defined(USE_SPINLOCK_DEBUG)
     lock->location = NULL;
 #endif
 }
+
+/*
+ * Internal lock debugging macros
+ */
+#if defined(USE_SPINLOCK_DEBUG)
+#define SPINLOCK_DEBUG_LOCATION SOURCE_LINE
+#define SPINLOCK_MARK_LOCATION(lock, location)                                                                         \
+    do {                                                                                                               \
+        lock->location = location;                                                                                     \
+    } while (0)
+#else
+#define SPINLOCK_DEBUG_LOCATION NULL
+#define SPINLOCK_MARK_LOCATION(lock, location)                                                                         \
+    do {                                                                                                               \
+        UNUSED(lock);                                                                                                  \
+        UNUSED(location);                                                                                              \
+    } while (0)
+#endif
 
 /*
  * Writer locking
@@ -59,27 +72,28 @@ static inline void spin_init(spinlock_t* lock)
 
 static forceinline bool spin_try_lock_internal(spinlock_t* lock, const char* location)
 {
-    bool ret = atomic_cas_uint32_ex(&lock->flag, 0x0, 0x1, false, ATOMIC_ACQUIRE, ATOMIC_RELAXED);
-#ifdef USE_SPINLOCK_DEBUG
-    if (likely(ret)) {
-        lock->location = location;
+    if (likely(atomic_cas_uint32_ex(&lock->flag, 0x0, 0x1, false, ATOMIC_ACQUIRE, ATOMIC_RELAXED))) {
+        SPINLOCK_MARK_LOCATION(lock, location);
+        return true;
     }
-#else
-    UNUSED(location);
-#endif
-    return ret;
+    return false;
 }
 
 static forceinline void spin_lock_internal(spinlock_t* lock, const char* location)
 {
-    if (unlikely(!spin_try_lock_internal(lock, location))) {
+    // Use weak CAS in fast path
+    if (likely(atomic_cas_uint32_ex(&lock->flag, 0x0, 0x1, true, ATOMIC_ACQUIRE, ATOMIC_RELAXED))) {
+        SPINLOCK_MARK_LOCATION(lock, location);
+    } else {
         spin_lock_wait(lock, location);
     }
 }
 
 static forceinline void spin_lock_slow_internal(spinlock_t* lock, const char* location)
 {
-    if (unlikely(!spin_try_lock_internal(lock, location))) {
+    if (likely(atomic_cas_uint32_ex(&lock->flag, 0x0, 0x1, true, ATOMIC_ACQUIRE, ATOMIC_RELAXED))) {
+        SPINLOCK_MARK_LOCATION(lock, location);
+    } else {
         spin_lock_wait(lock, NULL);
     }
 }
@@ -98,8 +112,8 @@ static forceinline void spin_lock_slow_internal(spinlock_t* lock, const char* lo
 static forceinline void spin_unlock(spinlock_t* lock)
 {
     uint32_t prev = atomic_swap_uint32_ex(&lock->flag, 0x0, ATOMIC_RELEASE);
-    if (unlikely(prev > 0x1)) {
-        // Waiters are present, or incorrectly locked as a reader
+    if (unlikely(prev != 0x1)) {
+        // Waiters are present, or invalid usage detected (Not locked / Locked as a reader)
         spin_lock_wake(lock, prev);
     }
 }
@@ -113,12 +127,12 @@ static forceinline bool spin_try_read_lock(spinlock_t* lock)
 {
     uint32_t prev;
     do {
-        prev = atomic_load_uint32_ex(&lock->flag, ATOMIC_RELAXED);
-        if (unlikely(prev && ((int32_t)prev) < 0x2)) {
+        prev = atomic_load_uint32_relax(&lock->flag);
+        if (unlikely(prev & 0x80000001U)) {
             // Writer owns the lock, writer waiters are present or too much readers (sic!)
             return false;
         }
-    } while (unlikely(!atomic_cas_uint32_ex(&lock->flag, prev, prev + 0x2, true, ATOMIC_ACQUIRE, ATOMIC_RELAXED)));
+    } while (!atomic_cas_uint32_ex(&lock->flag, prev, prev + 0x2, true, ATOMIC_ACQUIRE, ATOMIC_RELAXED));
     return true;
 }
 
@@ -139,9 +153,9 @@ static forceinline void spin_read_lock_internal(spinlock_t* lock, const char* lo
 // Release the reader lock
 static forceinline void spin_read_unlock(spinlock_t* lock)
 {
-    int32_t prev = atomic_sub_uint32_ex(&lock->flag, 0x2, ATOMIC_RELEASE);
-    if (unlikely(prev < 0x2)) {
-        // Waiters are present, or incorrectly locked as a writer
+    uint32_t prev = atomic_sub_uint32_ex(&lock->flag, 0x2, ATOMIC_RELEASE);
+    if (unlikely(((int32_t)prev) < 0x2)) {
+        // Waiters are present, or invalid usage detected (Not locked / Locked as a writer)
         spin_read_lock_wake(lock, prev);
     }
 }
