176119ce8b722a3760e103b3c1be47d024def809
LekKit <50500857+LekKit@users.noreply.github.com>
2025-06-17 16:20:38 +0300

    main: Remove "v" prefix in version string

--- src/main.c
+++ src/main.c
@@ -79,7 +79,7 @@ static void rvvm_print_help(void)
         "    ░           ░        ░         ░   \n"
         "               ░        ░              \n"
         "\n"
-        "https://github.com/LekKit/RVVM (v" RVVM_VERSION ")\n"
+        "https://github.com/LekKit/RVVM (" RVVM_VERSION ")\n"
         "\n"
         "License GPLv3+: GNU GPL version 3 or later <http://gnu.org/licenses/gpl.html>\n"
         "This is free software: you are free to change and redistribute it.\n"
