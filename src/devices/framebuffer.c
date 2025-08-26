7661f0e06ddde4bcb7d19ff684fee052b195bfae
LekKit <50500857+LekKit@users.noreply.github.com>
2025-08-26 14:41:49 +0300

    framebuffer: Optimize for size on Clang, apply format

--- src/devices/framebuffer.c
+++ src/devices/framebuffer.c
@@ -11,13 +11,15 @@ file, You can obtain one at https://mozilla.org/MPL/2.0/.
 #include "fdtlib.h"
 #include "utils.h"
 
+PUSH_OPTIMIZATION_SIZE
+
 static void fb_remove(rvvm_mmio_dev_t* dev)
 {
     UNUSED(dev);
 }
 
 static rvvm_mmio_type_t fb_dev_type = {
-    .name = "framebuffer",
+    .name   = "framebuffer",
     .remove = fb_remove,
 };
 
@@ -25,14 +27,16 @@ PUBLIC rvvm_mmio_dev_t* framebuffer_init(rvvm_machine_t* machine, rvvm_addr_t ad
 {
     // Map the framebuffer into physical memory
     rvvm_mmio_dev_t fb_mmio = {
-        .addr = addr,
-        .size = framebuffer_size(fb),
+        .addr    = addr,
+        .size    = framebuffer_size(fb),
         .mapping = fb->buffer,
-        .type = &fb_dev_type,
+        .type    = &fb_dev_type,
     };
 
     rvvm_mmio_dev_t* mmio = rvvm_attach_mmio(machine, &fb_mmio);
-    if (mmio == NULL) return mmio;
+    if (mmio == NULL) {
+        return mmio;
+    }
 
 #ifdef USE_FDT
     struct fdt_node* fb_fdt = fdt_node_create_reg("framebuffer", fb_mmio.addr);
@@ -55,7 +59,7 @@ PUBLIC rvvm_mmio_dev_t* framebuffer_init(rvvm_machine_t* machine, rvvm_addr_t ad
             rvvm_warn("Unknown RGB format in framebuffer_init()!");
             break;
     }
-    fdt_node_add_prop_u32(fb_fdt, "width",  fb->width);
+    fdt_node_add_prop_u32(fb_fdt, "width", fb->width);
     fdt_node_add_prop_u32(fb_fdt, "height", fb->height);
     fdt_node_add_prop_u32(fb_fdt, "stride", framebuffer_stride(fb));
 
@@ -67,10 +71,12 @@ PUBLIC rvvm_mmio_dev_t* framebuffer_init(rvvm_machine_t* machine, rvvm_addr_t ad
 
 PUBLIC rvvm_mmio_dev_t* framebuffer_init_auto(rvvm_machine_t* machine, const fb_ctx_t* fb)
 {
-    rvvm_addr_t addr = rvvm_mmio_zone_auto(machine, 0x18000000, framebuffer_size(fb));
+    rvvm_addr_t      addr = rvvm_mmio_zone_auto(machine, 0x18000000, framebuffer_size(fb));
     rvvm_mmio_dev_t* mmio = framebuffer_init(machine, addr, fb);
     if (mmio) {
         rvvm_append_cmdline(machine, "console=tty0");
     }
     return mmio;
 }
+
+POP_OPTIMIZATION_SIZE
