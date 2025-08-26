83a84b58222faff72091be7341228abec0611a8f
LekKit <50500857+LekKit@users.noreply.github.com>
2025-08-26 15:01:16 +0300

    ringbuf: Optimize for size on Clang, apply format

--- src/ringbuf.c
+++ src/ringbuf.c
@@ -9,23 +9,23 @@ file, You can obtain one at https://mozilla.org/MPL/2.0/.
 */
 
 #include "ringbuf.h"
-#include "utils.h"
 #include "mem_ops.h"
+#include "utils.h"
 
-SOURCE_OPTIMIZATION_SIZE
+PUSH_OPTIMIZATION_SIZE
 
 void ringbuf_create(ringbuf_t* rb, size_t size)
 {
-    rb->data = safe_new_arr(uint8_t, size);
-    rb->size = size;
-    rb->start = 0;
+    rb->data     = safe_new_arr(uint8_t, size);
+    rb->size     = size;
+    rb->start    = 0;
     rb->consumed = 0;
 }
 
 void ringbuf_destroy(ringbuf_t* rb)
 {
-    rb->size = 0;
-    rb->start = 0;
+    rb->size     = 0;
+    rb->start    = 0;
     rb->consumed = 0;
     free(rb->data);
 }
@@ -42,22 +42,20 @@ size_t ringbuf_avail(ringbuf_t* rb)
 
 size_t ringbuf_skip(ringbuf_t* rb, size_t len)
 {
-    size_t skip = EVAL_MIN(len, rb->consumed);
+    size_t skip   = EVAL_MIN(len, rb->consumed);
     rb->consumed -= skip;
     return skip;
 }
 
 static inline size_t ringbuf_get_read_start(ringbuf_t* rb)
 {
-    return rb->consumed > rb->start
-        ? (rb->size - rb->consumed + rb->start)
-        : (rb->start - rb->consumed);
+    return rb->consumed > rb->start ? (rb->size - rb->consumed + rb->start) : (rb->start - rb->consumed);
 }
 
 size_t ringbuf_peek(ringbuf_t* rb, void* data, size_t len)
 {
-    size_t start = ringbuf_get_read_start(rb);
-    size_t ret = EVAL_MIN(rb->consumed, len);
+    size_t start     = ringbuf_get_read_start(rb);
+    size_t ret       = EVAL_MIN(rb->consumed, len);
     size_t lhalf_len = EVAL_MIN(rb->size - start, ret);
     memcpy(data, ((uint8_t*)rb->data) + start, lhalf_len);
     if (ret > lhalf_len) {
@@ -76,7 +74,7 @@ size_t ringbuf_read(ringbuf_t* rb, void* data, size_t len)
 
 size_t ringbuf_write(ringbuf_t* rb, const void* data, size_t len)
 {
-    size_t ret = EVAL_MIN(rb->size - rb->consumed, len);
+    size_t ret       = EVAL_MIN(rb->size - rb->consumed, len);
     size_t lhalf_len = EVAL_MIN(rb->size - rb->start, ret);
     memcpy(((uint8_t*)rb->data) + rb->start, data, lhalf_len);
     if (ret > lhalf_len) {
@@ -105,7 +103,9 @@ bool ringbuf_put(ringbuf_t* rb, const void* data, size_t len)
         ringbuf_write(rb, data, len);
         return true;
     }
-    DO_ONCE(rvvm_info("Overflow in ring %p! (size: %u, consumed: %u, len: %u)",
-              (void*)rb, (uint32_t)rb->size, (uint32_t)rb->consumed, (uint32_t)len));
+    DO_ONCE(rvvm_info("Overflow in ring %p! (size: %u, consumed: %u, len: %u)", //
+                      (void*)rb, (uint32_t)rb->size, (uint32_t)rb->consumed, (uint32_t)len));
     return false;
 }
+
+POP_OPTIMIZATION_SIZE
