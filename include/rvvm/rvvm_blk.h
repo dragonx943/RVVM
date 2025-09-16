d6e43be53f200331505368e525217054e2fa3eef
LekKit <50500857+LekKit@users.noreply.github.com>
2025-09-16 17:46:10 +0300

    librvvm: Slight <rvvm/rvvm_blk.h> API changes
    - Get rid of block device image format registry for now, it is troublesome to implement and not worth it
    - Provide rvvm_blk_init(), rvvm_blk_register_cb(), rvvm_blk_set_data(), rvvm_blk_get_data() for custom image format implementations
    - Block device implementations should accept a rvvm_blk_dev_t* blk handle
    - Add whence argument to rvvm_blk_seek_head()
    - Minor renaming

--- include/rvvm/rvvm_blk.h
+++ include/rvvm/rvvm_blk.h
@@ -1,5 +1,5 @@
 /*
-<rvvm/rvvm_blk.h> - Block device IO
+<rvvm/rvvm_blk.h> - RVVM Block device IO
 Copyright (C) 2020-2025  LekKit <github.com/LekKit>
 
 This Source Code Form is subject to the terms of the Mozilla Public
@@ -24,12 +24,19 @@ RVVM_EXTERN_C_BEGIN
  * Options for rvvm_blk_open()
  */
 
-#define RVVM_BLK_READ   0x01 /**< Permit reading the block device image      */
-#define RVVM_BLK_WRITE  0x02 /**< Permit writing the block device image      */
-#define RVVM_BLK_RW     0x03 /**< Open block device image in read/write mode */
-#define RVVM_BLK_CREAT  0x04 /**< Create image if it doesn't exist           */
-#define RVVM_BLK_DIRECT 0x20 /**< Direct DMA IO with the underlying storage  */
-#define RVVM_BLK_SYNC   0x40 /**< Disable writeback buffering                */
+#define RVVM_BLK_READ     0x01 /**< Permit reading the block device image      */
+#define RVVM_BLK_WRITE    0x02 /**< Permit writing the block device image      */
+#define RVVM_BLK_RW       0x03 /**< Open block device image in read/write mode */
+#define RVVM_BLK_CREAT    0x04 /**< Create image if it doesn't exist           */
+#define RVVM_BLK_DIRECT   0x20 /**< Direct DMA IO with the underlying storage  */
+#define RVVM_BLK_SYNC     0x40 /**< Disable writeback buffering                */
+
+/*
+ * Seek whence for rvvm_blk_seek_head()
+ */
+#define RVVM_BLK_SEEK_SET 0x00 /** Set drive head position relative to image beginning  */
+#define RVVM_BLK_SEEK_CUR 0x01 /** Set drive head position relative to current position */
+#define RVVM_BLK_SEEK_END 0x02 /** Set drive head position relative to image end        */
 
 /**
  * Block device image handle
@@ -41,7 +48,7 @@ typedef struct rvvm_blk_dev rvvm_blk_dev_t;
  *
  * \param name Image name (File path, etc)
  * \param fmt  Specific format identifier, or NULL to probe a matching one
- * \param opts Open options (Read/Write/Create), zero implies RVVM_BLK_RW
+ * \param opts Open options (Read/Write/Create), zero implies RVVM_BLK_RW | RVVM_BLK_DIRECT
  * \return     Block device image handle, or NULL on failure
  */
 RVVM_PUBLIC rvvm_blk_dev_t* rvvm_blk_open(const char* name, const char* fmt, uint32_t opts);
@@ -49,67 +56,62 @@ RVVM_PUBLIC rvvm_blk_dev_t* rvvm_blk_open(const char* name, const char* fmt, uin
 /**
  * Close a block device image handle
  */
-RVVM_PUBLIC void rvvm_blk_close(rvvm_blk_dev_t* dev);
-
-/**
- * Get block device size in bytes
- */
-RVVM_PUBLIC uint64_t rvvm_blk_get_size(rvvm_blk_dev_t* dev);
+RVVM_PUBLIC void rvvm_blk_close(rvvm_blk_dev_t* blk);
 
 /**
  * Get block device format identifier string
  */
-RVVM_PUBLIC const char* rvvm_blk_get_format(rvvm_blk_dev_t* dev);
+RVVM_PUBLIC const char* rvvm_blk_get_format(rvvm_blk_dev_t* blk);
+
+/**
+ * Get block device size in bytes
+ */
+RVVM_PUBLIC uint64_t rvvm_blk_get_size(rvvm_blk_dev_t* blk);
 
 /**
  * Resize block device (If supported)
  */
-RVVM_PUBLIC bool rvvm_blk_set_size(rvvm_blk_dev_t* dev, uint64_t size);
+RVVM_PUBLIC bool rvvm_blk_set_size(rvvm_blk_dev_t* blk, uint64_t size);
 
 /**
  * Read data at specified offset, returns how much was actually read
  */
-RVVM_PUBLIC size_t rvvm_blk_read(rvvm_blk_dev_t* dev, void* dst, size_t size, uint64_t off);
+RVVM_PUBLIC size_t rvvm_blk_read(rvvm_blk_dev_t* blk, void* dst, size_t size, uint64_t off);
 
 /**
  * Write data at specified offset, returns how much was actually written
  */
-RVVM_PUBLIC size_t rvvm_blk_write(rvvm_blk_dev_t* dev, const void* src, size_t size, uint64_t off);
-
-/**
- * Seek drive head
- */
-RVVM_PUBLIC bool rvvm_blk_seek_head(rvvm_blk_dev_t* dev, uint64_t off);
-
-/**
- * Tell drive head position
- */
-RVVM_PUBLIC uint64_t rvvm_blk_tell_head(rvvm_blk_dev_t* dev);
-
-/**
- * Write data at drive head, returns how much was actually written
- */
-RVVM_PUBLIC size_t rvvm_blk_write_head(rvvm_blk_dev_t* dev, const void* src, size_t size);
-
-/**
- * Read data at drive head, returns how much was actually read
- */
-RVVM_PUBLIC size_t rvvm_blk_read_head(rvvm_blk_dev_t* dev, void* dst, size_t size);
-
-/**
- * Write data at drive head, returns how much was actually written
- */
-RVVM_PUBLIC size_t rvvm_blk_write_head(rvvm_blk_dev_t* dev, const void* src, size_t size);
+RVVM_PUBLIC size_t rvvm_blk_write(rvvm_blk_dev_t* blk, const void* src, size_t size, uint64_t off);
 
 /**
  * Synchronize / Barrier buffered writes to stable storage
  */
-RVVM_PUBLIC bool rvvm_blk_sync(rvvm_blk_dev_t* dev);
+RVVM_PUBLIC bool rvvm_blk_sync(rvvm_blk_dev_t* blk);
 
 /**
  * Trim (Deallocate) unused data (If supported)
  */
-RVVM_PUBLIC bool rvvm_blk_trim(rvvm_blk_dev_t* dev, uint64_t off, uint64_t size);
+RVVM_PUBLIC bool rvvm_blk_trim(rvvm_blk_dev_t* blk, uint64_t off, uint64_t size);
+
+/**
+ * Seek drive head
+ */
+RVVM_PUBLIC bool rvvm_blk_seek_head(rvvm_blk_dev_t* blk, int64_t off, uint32_t whence);
+
+/**
+ * Tell drive head position
+ */
+RVVM_PUBLIC uint64_t rvvm_blk_tell_head(rvvm_blk_dev_t* blk);
+
+/**
+ * Read data at drive head, returns how much was actually read
+ */
+RVVM_PUBLIC size_t rvvm_blk_read_head(rvvm_blk_dev_t* blk, void* dst, size_t size);
+
+/**
+ * Write data at drive head, returns how much was actually written
+ */
+RVVM_PUBLIC size_t rvvm_blk_write_head(rvvm_blk_dev_t* blk, const void* src, size_t size);
 
 /** @}*/
 
@@ -131,75 +133,63 @@ typedef struct {
     /**
      * Probe block device format
      */
-    bool (*probe)(rvvm_blk_dev_t* dev, const char* name, uint32_t opts);
+    bool (*probe)(rvvm_blk_dev_t* blk, const char* name, uint32_t opts);
 
     /**
      * Free private block device data
      */
-    void (*close)(rvvm_blk_dev_t* dev);
+    void (*close)(rvvm_blk_dev_t* blk);
 
     /**
      * Get block device size in bytes
      */
-    uint64_t (*get_size)(rvvm_blk_dev_t* dev);
+    uint64_t (*get_size)(rvvm_blk_dev_t* blk);
 
     /**
      * Resize block device (If supported)
      */
-    bool (*set_size)(rvvm_blk_dev_t* dev, uint64_t size);
+    bool (*set_size)(rvvm_blk_dev_t* blk, uint64_t size);
 
     /**
      * Read data at specified offset, returns how much was actually read
      */
-    size_t (*read)(rvvm_blk_dev_t* dev, void* dst, size_t size, uint64_t off);
+    size_t (*read)(rvvm_blk_dev_t* blk, void* dst, size_t size, uint64_t off);
 
     /**
      * Write data at specified offset, returns how much was actually written
      */
-    size_t (*write)(rvvm_blk_dev_t* dev, const void* src, size_t size, uint64_t off);
+    size_t (*write)(rvvm_blk_dev_t* blk, const void* src, size_t size, uint64_t off);
 
     /**
      * Synchronize / Barrier buffered writes to stable storage
      */
-    bool (*sync)(rvvm_blk_dev_t* dev);
+    bool (*sync)(rvvm_blk_dev_t* blk);
 
     /**
      * Trim (Deallocate) unused data (If supported)
      */
-    bool (*trim)(rvvm_blk_dev_t* dev, uint64_t off, uint64_t size);
+    bool (*trim)(rvvm_blk_dev_t* blk, uint64_t off, uint64_t size);
 } rvvm_blk_cb_t;
 
 /**
- * Register block device image format
- *
- * \param cb Block device handlers
- *
- * The structure must be available until rvvm_unregister_blk_format() is called.
- * It is preferable to simply statically allocate it, like this:
- *
- * static const rvvm_blk_cb_t blk_raw_cb = {
- *     .open = blk_raw_open,
- *     ...
- * };
+ * Create a dummy block device
  */
-RVVM_PUBLIC bool rvvm_register_blk_format(const rvvm_blk_cb_t* cb);
+RVVM_PUBLIC rvvm_blk_dev_t* rvvm_blk_init(void);
 
 /**
- * Unregister block device image format
- *
- * \param cb Block device handlers previously registered by rvvm_register_blk_format()
+ * Register block device image callbacks
  */
-RVVM_PUBLIC void rvvm_unregister_blk_format(const rvvm_blk_cb_t* cb);
+RVVM_PUBLIC void rvvm_blk_register_cb(rvvm_blk_dev_t* blk, const rvvm_blk_cb_t* cb, uint32_t opts);
 
 /**
- * Set private block device image format data (On probe)
+ * Set private block device image data
  */
-RVVM_PUBLIC void rvvm_blk_set_format_data(rvvm_blk_dev_t* dev, void* data);
+RVVM_PUBLIC void rvvm_blk_set_data(rvvm_blk_dev_t* blk, void* data);
 
 /**
- * Get private block device image format data
+ * Get private block device image data
  */
-RVVM_PUBLIC void* rvvm_blk_get_format_data(rvvm_blk_dev_t* dev);
+RVVM_PUBLIC void* rvvm_blk_get_data(rvvm_blk_dev_t* blk);
 
 /** @}*/
 
