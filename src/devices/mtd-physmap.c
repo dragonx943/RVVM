da27a4c9da7c9582cb73f5b961ce7bb2af007626
LekKit <50500857+LekKit@users.noreply.github.com>
2025-09-16 17:52:18 +0300

    mtd-physmap: Use new rvvm_blk API, buffering
    - Use a 4kb buffer to perform IO, read/write to that buffer

--- src/devices/mtd-physmap.c
+++ src/devices/mtd-physmap.c
@@ -8,70 +8,94 @@ file, You can obtain one at https://mozilla.org/MPL/2.0/.
 */
 
 #include "mtd-physmap.h"
-#include "blk_io.h"
+
 #include "fdtlib.h"
+#include "mem_ops.h"
 #include "utils.h"
 
 PUSH_OPTIMIZATION_SIZE
 
 typedef struct {
-    blkdev_t* blk;
-} mtd_dev_t;
+    rvvm_blk_dev_t* blk;
 
-static void mtd_remove(rvvm_mmio_dev_t* dev)
+    uint8_t  buffer[4096];
+    uint32_t dirty;
+} mtd_flash_t;
+
+static void mtd_flash_remove(rvvm_mmio_dev_t* dev)
 {
-    mtd_dev_t* mtd = dev->data;
-    blk_close(mtd->blk);
+    mtd_flash_t* mtd = dev->data;
+    if (atomic_load_uint32_relax(&mtd->dirty)) {
+        rvvm_blk_write_head(mtd->blk, mtd->buffer, sizeof(mtd->buffer));
+    }
+    rvvm_blk_close(mtd->blk);
     free(mtd);
 }
 
-static void mtd_reset(rvvm_mmio_dev_t* dev)
+static void mtd_flash_reset(rvvm_mmio_dev_t* dev)
 {
-    mtd_dev_t* mtd = dev->data;
-    void* ptr = rvvm_get_dma_ptr(dev->machine, rvvm_get_opt(dev->machine, RVVM_OPT_MEM_BASE), blk_getsize(mtd->blk));
+    mtd_flash_t* mtd  = dev->data;
+    rvvm_addr_t  addr = rvvm_get_opt(dev->machine, RVVM_OPT_RESET_PC);
+    rvvm_addr_t  size = rvvm_blk_get_size(mtd->blk);
+
+    void* ptr = rvvm_get_dma_ptr(dev->machine, addr, size);
     if (ptr) {
-        blk_read(mtd->blk, ptr, blk_getsize(mtd->blk), 0);
+        rvvm_blk_read(mtd->blk, ptr, size, 0);
     }
 }
 
-static rvvm_mmio_type_t mtd_type = {
-    .name   = "mtd_physmap",
-    .remove = mtd_remove,
-    .reset  = mtd_reset,
+static const rvvm_mmio_type_t mtd_flash_type = {
+    .name   = "mtd-physmap",
+    .remove = mtd_flash_remove,
+    .reset  = mtd_flash_reset,
 };
 
-static bool mtd_mmio_read(rvvm_mmio_dev_t* dev, void* data, size_t offset, uint8_t size)
+static bool mtd_flash_mmio_read(rvvm_mmio_dev_t* dev, void* data, size_t off, uint8_t size)
 {
-    mtd_dev_t* mtd = dev->data;
-    return blk_read(mtd->blk, data, size, offset) == size;
+    mtd_flash_t* mtd = dev->data;
+    if ((off >> 12) != (rvvm_blk_tell_head(mtd->blk) >> 12)) {
+        rvvm_blk_seek_head(mtd->blk, (off >> 12) << 12, RVVM_BLK_SEEK_SET);
+        rvvm_blk_read_head(mtd->blk, mtd->buffer, sizeof(mtd->buffer));
+    }
+    memcpy(data, mtd->buffer + (off & 0xFFF), size);
+    return true;
 }
 
-static bool mtd_mmio_write(rvvm_mmio_dev_t* dev, void* data, size_t offset, uint8_t size)
+static bool mtd_flash_mmio_write(rvvm_mmio_dev_t* dev, void* data, size_t off, uint8_t size)
 {
-    mtd_dev_t* mtd = dev->data;
-    return blk_write(mtd->blk, data, size, offset) == size;
+    mtd_flash_t* mtd = dev->data;
+    if ((off >> 12) != (rvvm_blk_tell_head(mtd->blk) >> 12)) {
+        rvvm_blk_write_head(mtd->blk, mtd->buffer, sizeof(mtd->buffer));
+        rvvm_blk_seek_head(mtd->blk, (off >> 12) << 12, RVVM_BLK_SEEK_SET);
+    }
+    memcpy(mtd->buffer + (off & 0xFFF), data, size);
+    atomic_store_uint32_relax(&mtd->dirty, true);
+    return true;
 }
 
-PUBLIC rvvm_mmio_dev_t* mtd_physmap_init_blk(rvvm_machine_t* machine, rvvm_addr_t addr, void* blk_dev)
+PUBLIC rvvm_mmio_dev_t* mtd_physmap_init_blk(rvvm_machine_t* machine, rvvm_addr_t addr, rvvm_blk_dev_t* blk)
 {
-    mtd_dev_t* mtd = safe_new_obj(mtd_dev_t);
-    mtd->blk       = blk_dev;
+    mtd_flash_t* mtd = safe_new_obj(mtd_flash_t);
+
+    mtd->blk = blk;
+
+    rvvm_blk_read_head(mtd->blk, mtd->buffer, sizeof(mtd->buffer));
 
     rvvm_mmio_dev_t mtd_mmio = {
         .addr        = addr,
-        .size        = blk_getsize(mtd->blk),
+        .size        = rvvm_blk_get_size(mtd->blk),
         .min_op_size = 1,
         .max_op_size = 8,
-        .read        = mtd_mmio_read,
-        .write       = mtd_mmio_write,
+        .read        = mtd_flash_mmio_read,
+        .write       = mtd_flash_mmio_write,
         .data        = mtd,
-        .type        = &mtd_type,
+        .type        = &mtd_flash_type,
     };
     rvvm_mmio_dev_t* mmio = rvvm_attach_mmio(machine, &mtd_mmio);
     if (mmio == NULL) {
         return mmio;
     }
-#ifdef USE_FDT
+#if defined(USE_FDT)
     struct fdt_node* mtd_fdt = fdt_node_create_reg("flash", mtd_mmio.addr);
     fdt_node_add_prop_reg(mtd_fdt, "reg", mtd_mmio.addr, mtd_mmio.size);
     fdt_node_add_prop_str(mtd_fdt, "compatible", "mtd-ram");
@@ -80,10 +104,7 @@ PUBLIC rvvm_mmio_dev_t* mtd_physmap_init_blk(rvvm_machine_t* machine, rvvm_addr_
     fdt_node_add_prop_u32(mtd_fdt, "#size-cells", 1);
     {
         struct fdt_node* partition0 = fdt_node_create("partition@0");
-        uint32_t         reg[2]     = {
-            0,
-            mtd_mmio.size,
-        };
+        uint32_t         reg[2]     = {0, mtd_mmio.size};
         fdt_node_add_prop_cells(partition0, "reg", reg, 2);
         fdt_node_add_prop_str(partition0, "label", "firmware");
         fdt_node_add_child(mtd_fdt, partition0);
@@ -93,18 +114,18 @@ PUBLIC rvvm_mmio_dev_t* mtd_physmap_init_blk(rvvm_machine_t* machine, rvvm_addr_
     return mmio;
 }
 
-PUBLIC rvvm_mmio_dev_t* mtd_physmap_init(rvvm_machine_t* machine, rvvm_addr_t addr, const char* image_path, bool rw)
+rvvm_mmio_dev_t* mtd_physmap_init(rvvm_machine_t* machine, rvvm_addr_t addr, const char* image, bool rw)
 {
-    blkdev_t* blk = blk_open(image_path, rw ? BLKDEV_RW : 0);
-    if (blk == NULL) {
-        return NULL;
+    rvvm_blk_dev_t* blk = rvvm_blk_open(image, NULL, rw ? RVVM_BLK_RW : RVVM_BLK_READ);
+    if (blk) {
+        return mtd_physmap_init_blk(machine, addr, blk);
     }
-    return mtd_physmap_init_blk(machine, addr, blk);
+    return NULL;
 }
 
-PUBLIC rvvm_mmio_dev_t* mtd_physmap_init_auto(rvvm_machine_t* machine, const char* image_path, bool rw)
+rvvm_mmio_dev_t* mtd_physmap_init_auto(rvvm_machine_t* machine, const char* image, bool rw)
 {
-    return mtd_physmap_init(machine, MTD_PHYSMAP_DEFAULT_MMIO, image_path, rw);
+    return mtd_physmap_init(machine, MTD_PHYSMAP_DEFAULT_MMIO, image, rw);
 }
 
 POP_OPTIMIZATION_SIZE
--- src/devices/mtd-physmap.h
+++ src/devices/mtd-physmap.h
@@ -10,7 +10,7 @@ file, You can obtain one at https://mozilla.org/MPL/2.0/.
 #ifndef RVVM_MTD_PHYSMAP_H
 #define RVVM_MTD_PHYSMAP_H
 
-#include "rvvmlib.h"
+#include <rvvm/rvvm_blk.h>
 
 /*
  * The main purpose of this device is to allow guests to flash
@@ -19,9 +19,10 @@ file, You can obtain one at https://mozilla.org/MPL/2.0/.
 
 #define MTD_PHYSMAP_DEFAULT_MMIO 0x04000000
 
-PUBLIC rvvm_mmio_dev_t* mtd_physmap_init_blk(rvvm_machine_t* machine, rvvm_addr_t addr, void* blk_dev);
-PUBLIC rvvm_mmio_dev_t* mtd_physmap_init(rvvm_machine_t* machine, rvvm_addr_t addr, const char* image_path, bool rw);
-PUBLIC rvvm_mmio_dev_t* mtd_physmap_init_auto(rvvm_machine_t* machine, const char* image_path, bool rw);
+RVVM_PUBLIC rvvm_mmio_dev_t* mtd_physmap_init_blk(rvvm_machine_t* machine, rvvm_addr_t addr, rvvm_blk_dev_t* blk);
+
+RVVM_PUBLIC rvvm_mmio_dev_t* mtd_physmap_init(rvvm_machine_t* machine, rvvm_addr_t addr, const char* image, bool rw);
+RVVM_PUBLIC rvvm_mmio_dev_t* mtd_physmap_init_auto(rvvm_machine_t* machine, const char* image, bool rw);
 
 #endif
 
