1ac913c7bf95e72469291e8be767c23f63cae2b8
LekKit <50500857+LekKit@users.noreply.github.com>
2025-11-19 17:20:08 +0200

    librvvm: [RFC] Redesign GPIO Mux API
    - GPIO handle may be attached to any pins in an existing GPIO group to implement muxing
    - All GPIO devices / controllers are equal in the GPIO group
    - Writing pins is broadcasted to GPIO devices attached to those pins
    - Attaching a GPIO handle to a group increments all other GPIO handles refcount, dropping a GPIO handle decrements all connected GPIO handles refcount

--- include/rvvm/rvvm_gpio.h
+++ include/rvvm/rvvm_gpio.h
@@ -19,15 +19,18 @@ RVVM_EXTERN_C_BEGIN
  * @addtogroup rvvm_gpio_api
  * @{
  *
- * GPIO Controller and GPIO User both hold a handle to rvvm_gpio_dev_t,
- * and may set up GPIO callbacks on reading/writing pins by another side.
+ * GPIO controller and GPIO devices hold a handle to rvvm_gpio_dev_t,
+ * and may set up GPIO callbacks on reading/writing their pins.
  *
- * Both may also bi-directionally read/write pins instead of callbacks,
+ * They may also bi-directionally read/write pins instead of callbacks,
  * which may be more suitable for certain API usecases.
+ *
+ * GPIO handles can be muxed to a subset of pins of the GPIO group.
  *
  * Supplying a mask to rvvm_gpio_set_pins() allows updating specific pins.
  *
  * The rvvm_gpio_dev_t context is reference-counted.
+ * Dropping a GPIO handle unreferences all muxed handles.
  */
 
 /**
@@ -52,7 +55,13 @@ typedef struct {
 RVVM_PUBLIC rvvm_gpio_dev_t* rvvm_gpio_init(void);
 
 /**
- * Increment GPIO reference count
+ * Attach GPIO handle to a GPIO group, muxing the GPIO bus
+ */
+RVVM_PUBLIC bool rvvm_gpio_attach(rvvm_gpio_dev_t* mux, rvvm_gpio_dev_t* gpio, //
+                                  uint32_t pin_off, uint32_t pin_cnt);
+
+/**
+ * Increment GPIO handle reference count
  */
 RVVM_PUBLIC void rvvm_gpio_inc_ref(rvvm_gpio_dev_t* gpio);
 
@@ -62,27 +71,27 @@ RVVM_PUBLIC void rvvm_gpio_inc_ref(rvvm_gpio_dev_t* gpio);
 RVVM_PUBLIC bool rvvm_gpio_dec_ref(rvvm_gpio_dev_t* gpio);
 
 /**
- * Set private GPIO user data
+ * Set private GPIO device data
  */
 RVVM_PUBLIC void rvvm_gpio_set_data(rvvm_gpio_dev_t* gpio, void* data);
 
 /**
- * Get private GPIO user data
+ * Get private GPIO device data
  */
 RVVM_PUBLIC void* rvvm_gpio_get_data(rvvm_gpio_dev_t* gpio);
 
 /**
- * Register GPIO user callbacks
+ * Register GPIO device callbacks
  */
 RVVM_PUBLIC void rvvm_gpio_register_cb(rvvm_gpio_dev_t* gpio, const rvvm_gpio_cb_t* cb);
 
 /**
- * Set GPIO pins inbound to the GPIO controller (With mask)
+ * Set GPIO pins outbound from this device (With mask)
  */
 RVVM_PUBLIC void rvvm_gpio_set_pins(rvvm_gpio_dev_t* gpio, uint32_t off, uint32_t pins, uint32_t mask);
 
 /**
- * Get GPIO pins outbound from the GPIO controller
+ * Get GPIO pins inbound to this device
  */
 RVVM_PUBLIC uint32_t rvvm_gpio_get_pins(rvvm_gpio_dev_t* gpio, uint32_t off);
 
@@ -103,57 +112,6 @@ static inline bool rvvm_gpio_get_pin(rvvm_gpio_dev_t* gpio, uint32_t pin)
     uint32_t pins = rvvm_gpio_get_pins(gpio, pin >> 5);
     return pins & (1U << (pin & 0x1FU));
 }
-
-/** @}*/
-
-/**
- * @defgroup rvvm_gpio_ctl_api GPIO Controller API
- * @addtogroup rvvm_gpio_ctl_api
- * @{
- */
-
-/**
- * Register GPIO controller callbacks
- */
-RVVM_PUBLIC void rvvm_gpio_ctl_register_cb(rvvm_gpio_dev_t* gpio, const rvvm_gpio_cb_t* cb);
-
-/**
- * Set private GPIO controller data
- */
-RVVM_PUBLIC void rvvm_gpio_ctl_set_data(rvvm_gpio_dev_t* gpio, void* data);
-
-/**
- * Get private GPIO controller data
- */
-RVVM_PUBLIC void* rvvm_gpio_ctl_get_data(rvvm_gpio_dev_t* gpio);
-
-/**
- * Set outbound GPIO controller pins, in case GPIO controller callbacks are not used
- */
-RVVM_PUBLIC void rvvm_gpio_ctl_set_pins(rvvm_gpio_dev_t* gpio, uint32_t off, uint32_t pins);
-
-/**
- * Get inbound GPIO controller pins, in case GPIO controller callbacks are not used
- */
-RVVM_PUBLIC uint32_t rvvm_gpio_ctl_get_pins(rvvm_gpio_dev_t* gpio, uint32_t off);
-
-/** @}*/
-
-/**
- * @defgroup rvvm_gpio_mux_api GPIO Mux API
- * @addtogroup rvvm_gpio_mux_api
- * @{
- */
-
-/**
- * Create a muxed GPIO splitter to be connected to the upstream GPIO controller
- */
-RVVM_PUBLIC rvvm_gpio_dev_t* rvvm_gpio_mux_init(void);
-
-/**
- * Create a muxed GPIO device connected to a GPIO mux
- */
-RVVM_PUBLIC rvvm_gpio_dev_t* rvvm_gpio_mux_attach(rvvm_gpio_dev_t* gpio, uint32_t pin_off, uint32_t pin_cnt);
 
 /** @}*/
 
