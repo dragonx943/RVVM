9cecfb927757f5e661c3c35b39f5dc3d77a29c47
LekKit <50500857+LekKit@users.noreply.github.com>
2026-05-02 22:34:09 +0300

    framebuffer: Use new RVVM APIs

--- src/devices/framebuffer.c
+++ src/devices/framebuffer.c
@@ -7,105 +7,106 @@ License, v. 2.0. If a copy of the MPL was not distributed with this
 file, You can obtain one at https://mozilla.org/MPL/2.0/.
 */
 
-#include "framebuffer.h"
+#include <rvvm/rvvm_board.h>
+#include <rvvm/rvvm_fb.h>
+#include <rvvm/rvvm_fdt.h>
+#include <rvvm/rvvm_region.h>
+#include <rvvm/rvvm_snapshot.h>
+
+#include <rvvm/rvvm.h>
+
 #include "compiler.h"
-#include "fdtlib.h"
 
 PUSH_OPTIMIZATION_SIZE
 
-static bool simplefb_write(rvvm_mmio_dev_t* dev, void* data, size_t offset, uint8_t size)
+static void simplefb_write(rvvm_reg_dev_t* dev, const void* data, size_t size, size_t off)
 {
-    rvvm_fbdev_t* fbdev = dev->data;
+    rvvm_fbdev_t* fbdev = rvvm_region_data(dev);
     rvvm_fbdev_dirty(fbdev);
-    UNUSED(data);
-    UNUSED(offset);
-    UNUSED(size);
-    return true;
+    UNUSED(data && size && off);
 }
 
-static void simplefb_remove(rvvm_mmio_dev_t* dev)
+static void simplefb_poll(rvvm_reg_dev_t* dev)
 {
-    rvvm_fbdev_t* fbdev = dev->data;
-    rvvm_fbdev_dec_ref(fbdev);
-}
-
-static void simplefb_update(rvvm_mmio_dev_t* dev)
-{
-    rvvm_fbdev_t* fbdev = dev->data;
+    rvvm_fbdev_t* fbdev = rvvm_region_data(dev);
     rvvm_fbdev_update(fbdev);
 }
 
-static rvvm_mmio_type_t simplefb_dev_type = {
-    .name   = "simple-framebuffer",
-    .remove = simplefb_remove,
-    .update = simplefb_update,
+static void simplefb_suspend(rvvm_reg_dev_t* dev, rvvm_snapshot_t* snap, bool resume)
+{
+    rvvm_fb_t fb = ZERO_INIT;
+    if (snap) {
+        rvvm_fbdev_t* fbdev = rvvm_region_data(dev);
+        rvvm_fbdev_get_scanout(fbdev, &fb);
+        rvvm_snapshot_section(snap, "framebuffer");
+        rvvm_snapshot_data(snap, rvvm_fb_buffer(&fb), rvvm_fb_size(&fb));
+    }
+    UNUSED(resume);
+}
+
+static void simplefb_cleanup(rvvm_reg_dev_t* dev)
+{
+    rvvm_fbdev_t* fbdev = rvvm_region_data(dev);
+    rvvm_fbdev_dec_ref(fbdev);
+}
+
+static const rvvm_reg_type_t simplefb_type = {
+    .name    = "framebuffer",
+    .write   = simplefb_write,
+    .poll    = simplefb_poll,
+    .suspend = simplefb_suspend,
+    .cleanup = simplefb_cleanup,
 };
 
-PUBLIC rvvm_mmio_dev_t* rvvm_simplefb_init(rvvm_machine_t* machine, rvvm_addr_t addr, rvvm_fbdev_t* fbdev)
+RVVM_PUBLIC rvvm_reg_dev_t* rvvm_simplefb_init(rvvm_machine_t* machine, rvvm_fbdev_t* fbdev, rvvm_addr_t addr)
 {
-    if (!fbdev) {
+    rvvm_fb_t fb = ZERO_INIT;
+    rvvm_fbdev_get_scanout(fbdev, &fb);
+
+    if (!rvvm_fb_size(&fb) || !rvvm_fb_buffer(&fb)) {
         return NULL;
     }
 
-    rvvm_fb_t fb = ZERO_INIT;
-    rvvm_fbdev_get_scanout(fbdev, &fb);
-
-    // Map the framebuffer into physical memory
-    rvvm_mmio_dev_t simplefb_mmio = {
-        .addr    = addr,
-        .size    = rvvm_fb_size(&fb),
-        .data    = fbdev,
-        .mapping = rvvm_fb_buffer(&fb),
-        .type    = &simplefb_dev_type,
-        .write   = simplefb_write,
+    rvvm_reg_desc_t desc = {
+        .addr = addr,
+        .size = rvvm_fb_size(&fb),
+        .data = fbdev,
+        .mmap = rvvm_fb_buffer(&fb),
+        .type = &simplefb_type,
     };
 
-    rvvm_mmio_dev_t* mmio = rvvm_attach_mmio(machine, &simplefb_mmio);
-    if (mmio == NULL) {
-        return mmio;
+    rvvm_reg_dev_t*  dev = rvvm_region_init_auto(machine, &desc);
+    rvvm_fdt_node_t* soc = rvvm_get_fdt_soc(machine);
+
+    if (dev && soc) {
+        rvvm_fdt_node_t* fdt = rvvm_fdt_init_reg("framebuffer", desc.addr);
+        rvvm_fdt_prop_set_reg(fdt, "reg", desc.addr, desc.size);
+        rvvm_fdt_prop_set_str(fdt, "compatible", "simple-framebuffer");
+        switch (rvvm_fb_format(&fb)) {
+            case RVVM_RGB_RGB565:
+                rvvm_fdt_prop_set_str(fdt, "format", "r5g6b5");
+                break;
+            case RVVM_RGB_RGB888:
+                rvvm_fdt_prop_set_str(fdt, "format", "r8g8b8");
+                break;
+            case RVVM_RGB_BGR888:
+                rvvm_fdt_prop_set_str(fdt, "format", "b8g8r8");
+                break;
+            case RVVM_RGB_XRGB8888:
+                rvvm_fdt_prop_set_str(fdt, "format", "a8r8g8b8");
+                break;
+            case RVVM_RGB_XBGR8888:
+                rvvm_fdt_prop_set_str(fdt, "format", "a8b8g8r8");
+                break;
+        }
+        rvvm_fdt_prop_set_u32(fdt, "width", rvvm_fb_width(&fb));
+        rvvm_fdt_prop_set_u32(fdt, "height", rvvm_fb_height(&fb));
+        rvvm_fdt_prop_set_u32(fdt, "stride", rvvm_fb_stride(&fb));
+
+        rvvm_fdt_attach(soc, fdt);
     }
 
-#if defined(USE_FDT)
-    struct fdt_node* fb_fdt = fdt_node_create_reg("framebuffer", simplefb_mmio.addr);
-    fdt_node_add_prop_reg(fb_fdt, "reg", simplefb_mmio.addr, simplefb_mmio.size);
-    fdt_node_add_prop_str(fb_fdt, "compatible", "simple-framebuffer");
-    switch (rvvm_fb_format(&fb)) {
-        case RVVM_RGB_RGB565:
-            fdt_node_add_prop_str(fb_fdt, "format", "r5g6b5");
-            break;
-        case RVVM_RGB_RGB888:
-            fdt_node_add_prop_str(fb_fdt, "format", "r8g8b8");
-            break;
-        case RVVM_RGB_BGR888:
-            fdt_node_add_prop_str(fb_fdt, "format", "b8g8r8");
-            break;
-        case RVVM_RGB_XRGB8888:
-            fdt_node_add_prop_str(fb_fdt, "format", "a8r8g8b8");
-            break;
-        case RVVM_RGB_XBGR8888:
-            fdt_node_add_prop_str(fb_fdt, "format", "a8b8g8r8");
-            break;
-    }
-    fdt_node_add_prop_u32(fb_fdt, "width", rvvm_fb_width(&fb));
-    fdt_node_add_prop_u32(fb_fdt, "height", rvvm_fb_height(&fb));
-    fdt_node_add_prop_u32(fb_fdt, "stride", rvvm_fb_stride(&fb));
-
-    fdt_node_add_child(rvvm_get_fdt_soc(machine), fb_fdt);
-#endif
-
-    return mmio;
-}
-
-RVVM_PUBLIC rvvm_mmio_dev_t* rvvm_simplefb_init_auto(rvvm_machine_t* machine, rvvm_fbdev_t* fbdev)
-{
-    rvvm_fb_t fb = ZERO_INIT;
-    rvvm_fbdev_get_scanout(fbdev, &fb);
-    rvvm_addr_t      addr = rvvm_mmio_zone_auto(machine, 0x18000000, rvvm_fb_size(&fb));
-    rvvm_mmio_dev_t* mmio = rvvm_simplefb_init(machine, addr, fbdev);
-    if (mmio) {
-        rvvm_append_cmdline(machine, "console=tty0");
-    }
-    return mmio;
+    return dev;
 }
 
 POP_OPTIMIZATION_SIZE
--- src/devices/framebuffer.h
+++ src/devices/framebuffer.h
@@ -10,13 +10,21 @@ file, You can obtain one at https://mozilla.org/MPL/2.0/.
 #ifndef RVVM_FRAMEBUFFER_H
 #define RVVM_FRAMEBUFFER_H
 
-#include <rvvm/rvvm_fb.h>
+#include <rvvm/rvvm.h>
 
-/**
- * Attach fbdev to the machine as simple-framebuffer device
+/*
+ * TODO: Remove this in favor of <rvvm/rvvm_board.h>
  */
-RVVM_PUBLIC rvvm_mmio_dev_t* rvvm_simplefb_init(rvvm_machine_t* machine, rvvm_addr_t addr, rvvm_fbdev_t* fbdev);
 
-RVVM_PUBLIC rvvm_mmio_dev_t* rvvm_simplefb_init_auto(rvvm_machine_t* machine, rvvm_fbdev_t* fbdev);
+RVVM_PUBLIC rvvm_reg_dev_t* rvvm_simplefb_init(rvvm_machine_t* machine, rvvm_fbdev_t* fbdev, rvvm_addr_t addr);
+
+static inline rvvm_reg_dev_t* rvvm_simplefb_init_auto(rvvm_machine_t* machine, rvvm_fbdev_t* fbdev)
+{
+    rvvm_reg_dev_t* ret = rvvm_simplefb_init(machine, fbdev, 0x18000000UL);
+    if (ret) {
+        rvvm_append_cmdline(machine, "console=tty0");
+    }
+    return ret;
+}
 
 #endif
