f788ad517ae625267e0d4f4a6241f03f795c7703
LekKit <50500857+LekKit@users.noreply.github.com>
2026-05-02 22:36:59 +0300

    syscon: Use new RVVM APIs

--- src/devices/syscon.c
+++ src/devices/syscon.c
@@ -7,8 +7,12 @@ License, v. 2.0. If a copy of the MPL was not distributed with this
 file, You can obtain one at https://mozilla.org/MPL/2.0/.
 */
 
-#include "syscon.h"
-#include "fdtlib.h"
+#include <rvvm/rvvm_board.h>
+#include <rvvm/rvvm_fdt.h>
+#include <rvvm/rvvm_region.h>
+
+#include <rvvm/rvvm.h>
+
 #include "mem_ops.h"
 
 PUSH_OPTIMIZATION_SIZE
@@ -16,66 +20,58 @@ PUSH_OPTIMIZATION_SIZE
 #define SYSCON_POWEROFF 0x5555
 #define SYSCON_RESET    0x7777
 
-static bool syscon_mmio_write(rvvm_mmio_dev_t* dev, void* data, size_t offset, uint8_t size)
+static void syscon_mmio_write(rvvm_reg_dev_t* dev, const void* data, size_t size, size_t off)
 {
+    rvvm_machine_t* machine = rvvm_region_machine(dev);
+    uint16_t        val     = read_uint16_le(data);
     UNUSED(size);
-    if (offset == 0) {
-        switch (read_uint16_le_m(data)) {
-            case SYSCON_POWEROFF:
-            case SYSCON_RESET:
-                rvvm_reset_machine(dev->machine, read_uint16_le_m(data) == SYSCON_RESET);
-                break;
-        }
+
+    if (!off && val == SYSCON_POWEROFF) {
+        rvvm_reset_machine(machine, false);
+    } else if (!off && val == SYSCON_RESET) {
+        rvvm_reset_machine(machine, true);
     }
-    return true;
 }
 
-static rvvm_mmio_type_t syscon_dev_type = {
-    .name = "syscon",
+static const rvvm_reg_type_t syscon_type = {
+    .name     = "syscon",
+    .write    = syscon_mmio_write,
+    .min_size = 2,
+    .max_size = 2,
 };
 
-PUBLIC rvvm_mmio_dev_t* syscon_init(rvvm_machine_t* machine, rvvm_addr_t base_addr)
+RVVM_PUBLIC rvvm_reg_dev_t* rvvm_syscon_init(rvvm_machine_t* machine, rvvm_addr_t addr)
 {
-    rvvm_mmio_dev_t syscon = {
-        .addr        = base_addr,
-        .size        = 0x1000,
-        .type        = &syscon_dev_type,
-        .read        = rvvm_mmio_none,
-        .write       = syscon_mmio_write,
-        .min_op_size = 2,
-        .max_op_size = 2,
+    rvvm_reg_desc_t desc = {
+        .addr = addr,
+        .size = 0x1000,
+        .type = &syscon_type,
     };
-    rvvm_mmio_dev_t* mmio = rvvm_attach_mmio(machine, &syscon);
-    if (mmio == NULL) {
-        return mmio;
+
+    rvvm_reg_dev_t*  dev = rvvm_region_init_auto(machine, &desc);
+    rvvm_fdt_node_t* soc = rvvm_get_fdt_soc(machine);
+
+    if (dev && soc) {
+        rvvm_fdt_node_t* test = rvvm_fdt_init_reg("test", desc.addr);
+        rvvm_fdt_prop_set_reg(test, "reg", desc.addr, desc.size);
+        rvvm_fdt_prop_set_str_list(test, "compatible", "sifive,test1\0sifive,test0\0syscon");
+        rvvm_fdt_attach(soc, test);
+
+        rvvm_fdt_node_t* poweroff = rvvm_fdt_init("poweroff");
+        rvvm_fdt_prop_set_str(poweroff, "compatible", "syscon-poweroff");
+        rvvm_fdt_prop_set_u32(poweroff, "value", SYSCON_POWEROFF);
+        rvvm_fdt_prop_set_u32(poweroff, "offset", 0);
+        rvvm_fdt_prop_set_u32(poweroff, "regmap", rvvm_fdt_phandle(test));
+        rvvm_fdt_attach(rvvm_get_fdt_root(machine), poweroff);
+
+        rvvm_fdt_node_t* reboot = rvvm_fdt_init("reboot");
+        rvvm_fdt_prop_set_str(reboot, "compatible", "syscon-reboot");
+        rvvm_fdt_prop_set_u32(reboot, "value", SYSCON_RESET);
+        rvvm_fdt_prop_set_u32(reboot, "offset", 0);
+        rvvm_fdt_prop_set_u32(reboot, "regmap", rvvm_fdt_phandle(test));
+        rvvm_fdt_attach(rvvm_get_fdt_root(machine), reboot);
     }
-#ifdef USE_FDT
-    struct fdt_node* test = fdt_node_create_reg("test", base_addr);
-    fdt_node_add_prop_reg(test, "reg", base_addr, 0x1000);
-    fdt_node_add_prop(test, "compatible", "sifive,test1\0sifive,test0\0syscon\0", 33);
-    fdt_node_add_child(rvvm_get_fdt_soc(machine), test);
-
-    struct fdt_node* poweroff = fdt_node_create("poweroff");
-    fdt_node_add_prop_str(poweroff, "compatible", "syscon-poweroff");
-    fdt_node_add_prop_u32(poweroff, "value", SYSCON_POWEROFF);
-    fdt_node_add_prop_u32(poweroff, "offset", 0);
-    fdt_node_add_prop_u32(poweroff, "regmap", fdt_node_get_phandle(test));
-    fdt_node_add_child(rvvm_get_fdt_root(machine), poweroff);
-
-    struct fdt_node* reboot = fdt_node_create("reboot");
-    fdt_node_add_prop_str(reboot, "compatible", "syscon-reboot");
-    fdt_node_add_prop_u32(reboot, "value", SYSCON_RESET);
-    fdt_node_add_prop_u32(reboot, "offset", 0);
-    fdt_node_add_prop_u32(reboot, "regmap", fdt_node_get_phandle(test));
-    fdt_node_add_child(rvvm_get_fdt_root(machine), reboot);
-#endif
-    return mmio;
-}
-
-PUBLIC rvvm_mmio_dev_t* syscon_init_auto(rvvm_machine_t* machine)
-{
-    rvvm_addr_t addr = rvvm_mmio_zone_auto(machine, SYSCON_DEFAULT_MMIO, 0x1000);
-    return syscon_init(machine, addr);
+    return dev;
 }
 
 POP_OPTIMIZATION_SIZE
--- src/devices/syscon.h
+++ src/devices/syscon.h
@@ -10,11 +10,17 @@ file, You can obtain one at https://mozilla.org/MPL/2.0/.
 #ifndef RVVM_SYSCON_H
 #define RVVM_SYSCON_H
 
-#include "rvvmlib.h"
+#include <rvvm/rvvm_base.h>
 
-#define SYSCON_DEFAULT_MMIO 0x100000
+/*
+ * TODO: Remove this in favor of <rvvm/rvvm_board.h>
+ */
 
-PUBLIC rvvm_mmio_dev_t* syscon_init(rvvm_machine_t* machine, rvvm_addr_t base_addr);
-PUBLIC rvvm_mmio_dev_t* syscon_init_auto(rvvm_machine_t* machine);
+RVVM_PUBLIC rvvm_reg_dev_t* rvvm_syscon_init(rvvm_machine_t* machine, rvvm_addr_t base_addr);
+
+static inline rvvm_reg_dev_t* syscon_init_auto(rvvm_machine_t* machine)
+{
+    return rvvm_syscon_init(machine, 0x00100000UL);
+}
 
 #endif
